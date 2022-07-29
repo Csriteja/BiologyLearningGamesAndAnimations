3320dcb1399e162e7fa86a4023af57abf5721693
Csriteja <csriteja.1110@gmail.com>
2022-07-29 02:27:51 -0500

    Added prediction time animation in ek_predictingtraits section in genetics module

---
+++ BiologyAnimation/Genetics/ek_predictingtraits/PredictionTime/js/parameters.js
@@ -0,0 +1,39 @@
+var data = [
+	{
+		"id": 1,
+		"value": ""
+	},
+	{
+		"id": 2,
+		"value": "W"
+	},
+	{
+		"id": 3,
+		"value": "w"
+	},
+	{
+		"id": 4,
+		"value": "W"
+	},
+	{
+		"id": 5,
+		"value": "WW"
+	},
+	{
+		"id": 6,
+		"value": "Ww"
+	},
+	{
+		"id": 7,
+		"value": "w"
+	},
+	{
+		"id": 8,
+		"value": "Ww"
+	},
+	{
+		"id": 9,
+		"value": "ww"
+	}
+];	
+	
