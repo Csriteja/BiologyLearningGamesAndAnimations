6fcb8025d5fa6a3ae113defdf2747a65d535135c
Csriteja <csriteja.1110@gmail.com>
2022-07-29 01:20:40 -0500

    Added variations in traits and classification in bacteria animations

---
+++ BiologyAnimation/Genetics/ek_variationintraits/js/parameters.js
@@ -0,0 +1,35 @@
+var data = [
+	{
+		"id": 1,
+		"value": "Codominant alleles"
+	},
+	{
+		"id": 2,
+		"value": "Incomplete Dominance"
+	},
+	{
+		"id": 3,
+		"value": "Mutation"
+	},
+	{
+		"id": 4,
+		"value": "Multiple alleles"
+	},
+	{
+		"id": 5,
+		"value": "Sunlight"
+	},
+	{
+		"id": 6,
+		"value": "Radiation"
+	},
+	{
+		"id": 7,
+		"value": "Nutrition"
+	},
+	{
+		"id": 8,
+		"value": "Exercise"
+	}
+];	
+	
