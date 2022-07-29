2dcd2ab7f11deb0312f4de51566e635ab927f678
Csriteja <csriteja.1110@gmail.com>
2022-07-29 01:37:04 -0500

    Added checkpoint animation in asexual_vs_sexual reproduction module

---
+++ BiologyAnimation/Genetics/ek_asexualsexualreproduction/checkpoint/js/parameters.js
@@ -0,0 +1,35 @@
+var data = [
+	{
+		"id": 1,
+		"value": "Mitosis"
+	},
+	{
+		"id": 2,
+		"value": "Diploid"
+	},
+	{
+		"id": 3,
+		"value": "Identical"
+	},
+	{
+		"id": 4,
+		"value": "2 cells formed"
+	},
+	{
+		"id": 5,
+		"value": "Meiosis"
+	},
+	{
+		"id": 6,
+		"value": "Haploid"
+	},
+	{
+		"id": 7,
+		"value": "Diverse"
+	},
+	{
+		"id": 8,
+		"value": "4 cells formed"
+	}
+];	
+	
