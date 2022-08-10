047ad484fc622a5bc83a5def516f25beb1b12f7c
Csriteja <csriteja.1110@gmail.com>
2022-08-10 16:57:05 -0500

    Added drag and drop animation in dna_alleles section in Genetics Module

---
+++ BiologyAnimation/Genetics/ek_dnaalleles/js/parameters.js
@@ -0,0 +1,29 @@
+var data = [ 
+{	
+	"id" : "a1",
+	"value" : "Cell"
+},
+
+{	
+	"id" : "a2",
+	"value" : "Nucleus"
+},
+
+{	
+	"id" : "a3",
+	"value" : "Chromosomes"
+},
+
+{
+	"id" : "a4",
+	"value" : "Protein"
+},
+{
+	"id" : "a5",
+	"value" : "DNA"
+},
+{
+	"id" : "a6",
+	"value" : "Genes"
+}
+];
