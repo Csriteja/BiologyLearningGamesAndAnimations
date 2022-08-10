d4310a44719fb39fe7f64d634f89d813abbd3cda
Csriteja <csriteja.1110@gmail.com>
2022-08-10 17:50:29 -0500

    Added drag and drop animation in symbiotic_relationship section of Zoonotic Diseases module

---
+++ BiologyAnimation/Zoonotic Diseases/ek_symbioticrelationship/js/contents.js
@@ -0,0 +1,29 @@
+var questions = [
+{
+	"question id" : 1
+},
+
+{
+	"question id" : 2
+},
+
+{
+	"question id" : 3
+},
+
+{
+	"question id" : 4
+},
+
+{
+	"question id" : 5
+},
+{
+	"question id" : 6
+}
+
+];
+
+var slides_info = {
+    "hint": "Go back and read previous slides"
+}
